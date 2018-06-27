52e41b56402a9f20dc1c56d4ca629504a40d5622
vin <vincent.hilton@woodlanddirect.com>
2018-06-27 15:28:29 -0400

    refactored and adde the diff hero stuff

--- src/comps/LabelAndInput.jsx
+++ src/comps/LabelAndInput.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import Input from '../styled-comps/Input'
-import Title from '../styled-comps/Title'
+import { Input } from '../styled-comps/StyleLibrary'
+import { Title } from '../styled-comps/StyleLibrary'
 
 const InputLabel = styled.div`
 padding-bottom: 2px;
 `
 
-
 const LabelAndInput = props => {
   return (
     <div style={props.containerStyle}>
-    <InputLabel>{props.label}</InputLabel>
-    <Input
-      name={props.name}
-      value={props.value}
-      onChange={props.onChange}
-    />
+      <InputLabel>{props.label}</InputLabel>
+      <Input name={props.name} value={props.value} onChange={props.onChange} />
     </div>
   )
 }
---
+++ src/utils.js
@@ -0,0 +1,189 @@
+export const OG_DATA = {
+  hero: {
+    mob: {
+      image: 'https://assets.woodlanddirect.com/home-page/2018.July_MobileFriendly.Homepage_Banner.4thOfJulySale_A03.jpg',
+      link: '/Current-Promos'
+    },
+    tab: {
+      image: 'https://assets.woodlanddirect.com/home-page/2018.July_MobileFriendly.Homepage_Banner.4thOfJulySale_A03.jpg',
+      link: '/Current-Promos'
+    },
+    desk: {
+      image: 'https://assets.woodlanddirect.com/home-page/2018.July_MobileFriendly.Homepage_Banner.4thOfJulySale_A03.jpg',
+      link: '/Current-Promos'
+    }
+  },
+  spotlight: {
+    title: 'Spotlight',
+    items: [
+      {
+        image: 'www.starwars.com',
+        link: 'www.starwars.com',
+        text: 'spotlight'
+      }
+    ]
+  },
+  cats: {
+    title: 'Categories',
+    items: [
+      {
+        image: 'www.starwars.com',
+        link: 'www.starwars.com',
+        text: 'Fireplace'
+      },
+      {
+        image: 'www.starwars.com',
+        link: 'www.starwars.com',
+        text: 'Woood Stove'
+      },
+      {
+        image: 'www.starwars.com',
+        link: 'www.starwars.com',
+        text: 'Chimney'
+      },
+      {
+        image: 'www.starwars.com',
+        link: 'www.starwars.com',
+        text: 'Outdoor'
+      },
+      {
+        image: 'www.starwars.com',
+        link: 'www.starwars.com',
+        text: 'Promotions',
+        order: 5
+      }
+    ]
+  },
+  trending: {
+    title: 'Trending Now',
+    items: [
+      {
+        image: '',
+        link: '',
+        text: 'trending'
+      }
+    ]
+  }
+}
+
+export const DATA = {
+  hero: {
+    mob: {
+      image: 'https://assets.woodlanddirect.com/home-page/2018.July_MobileFriendly.Homepage_Banner.4thOfJulySale_A03.jpg',
+      link: '/Current-Promos'
+    },
+    tab: {
+      image: 'https://assets.woodlanddirect.com/home-page/2018.July_MobileFriendly.Homepage_Banner.4thOfJulySale_A03.jpg',
+      link: '/Current-Promos'
+    },
+    desk: {
+      image: 'https://assets.woodlanddirect.com/home-page/2018.July_MobileFriendly.Homepage_Banner.4thOfJulySale_A03.jpg',
+      link: '/Current-Promos'
+    }
+  },
+  cats: {
+    title: 'Categories',
+    items: [
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Category_Fireplaces2.jpg',
+        link: '/Fireplace-Accessories',
+        text: 'Fireplaces'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Category_FirePits2.jpg',
+        link: '/Outdoor/Outdoor-Fire-Pits',
+        text: 'Fire Pits'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Category_BBQGrills2.jpg',
+        link: '/Outdoor/Outdoor-BBQ-Grills',
+        text: 'BBQ Grills'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Category_IslandsAndKitchens2.jpg',
+        link: '/Outdoor/Outdoor-Kitchens-BBQ-Islands',
+        text: 'BBQ Islands & Kitchens'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Category_Chimneys2.jpg',
+        link: '/Chimney',
+        text: 'Chimney & Venting'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Category_GasLogs3.jpg',
+        link: '/Fireplace-Accessories/Gas-Logs',
+        text: 'Gas Logs'
+      }
+    ]
+  },
+  spotlight: {
+    title: 'Spotlight',
+    items: [
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Spotlight_A02.jpg',
+        link: '/Outdoor/Outdoor-Fireplace-Information/Outdoor-Fireplace-Gas-Types',
+        text: ''
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Spotlight_B02.1.jpg',
+        link: '/Ready-to-Finish-Outdoor-Kitchens',
+        text: ''
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Spotlight_C02.jpg',
+        link: '/Outdoor/The-Outdoor-GreatRoom-Outdoor-Fireplaces',
+        text: ''
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/MobileFriendly_Homepage.Spotlight_D02.jpg',
+        link: '/Current-Promos',
+        text: ''
+      }
+    ]
+  },
+  trending: {
+    title: 'Trending Now',
+    items: [
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_EmpireBoulevardContemporaryDirect-VentFireplace-600px.jpg',
+        link: '/Fireplace-Accessories/Direct-Vent-Fireplaces/Empire-Boulevard-Direct-Vent-Linear-Gas-Fireplace-60',
+        text: 'Empire Boulevard 60" Direct-Vent Fireplace'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_MontegoGasFirePitCoffeeTable-600px.jpg',
+        link: '/Outdoor/Fire-Pit-Tables/Montego-Gas-Fire-Pit-Coffee-Table-Brown',
+        text: 'Montego Gas Fire Pit Coffee Table'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_CoveGasFireBowl30-600px.jpg',
+        link: '/Outdoor/Gas-Fire-Bowls-Columns/Cove-Gas-Fire-Bowl-30',
+        text: 'Cove Gas Fire Bowl - 30"'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_BullOutdoorGourmet-QGrillingIsland-600px.jpg',
+        link: '/Outdoor/L-Shaped-Outdoor-Kitchens/Bull-Outdoor-Gourmet-Q-Grilling-Island-w-Built-In-Grill',
+        text: 'Bull Outdoor Gourmet-Q Grilling Island'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_Empire_Boulevard60ContemporaryVentlessFireplace-600px.jpg',
+        link: '/Fireplace-Accessories/Ventless-Fireplaces/Empire-Boulevard-Ventless-Linear-Gas-Fireplace-60',
+        text: 'Boulevard 60" Ventless Fireplace'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_UptownGasFirePitTableBlack42-600px.jpg',
+        link: '/Outdoor/Fire-Pit-Tables/Uptown-Gas-Fire-Pit-Table-Black-42-Linear-Burner',
+        text: 'Uptown Gas Fire Pit Table'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_NapoleonLV50NVector50DirectVentFireplace-600px.jpg',
+        link: '/Napoleon-LV50N-Vector-50-Direct-Vent-Gas-Fireplace',
+        text: 'Napoleon Vector 50 Direct Vent Fireplace'
+      },
+      {
+        image: 'https://assets.woodlanddirect.com/home-page/trend_BullOutdoorMaster-QGrillingIsland-600px.jpg',
+        link: '/Outdoor/Grilling-Islands/Bull-Outdoor-Master-Q-Grilling-Island-w-Built-In-Grill',
+        text: 'Bull Outdoor Master-Q Grilling Island'
+      }
+    ]
+  }
+}
